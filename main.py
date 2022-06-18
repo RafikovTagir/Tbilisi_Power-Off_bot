7cb5f5ae284255d3ada9e04f35274bd944845c21
RafikovTagir <RafikovTagir@gmail.com>
2022-06-18 22:43:00 +0400

    refactor context argument out

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def is_address_in_page(url, address):
         return response.text[left_p:right_p]
 
 
-def check_address(update, context):
+def check_address(update):
     user_id = update.message.from_user.id
     username = update.message.from_user.username
     db_object.execute(f'SELECT id FROM users WHERE id = {user_id}')
@@ -52,7 +52,7 @@ def start(update):
     update.message.reply_text('this bot can monitor websites and check for updated information on them')
 
 
-def bop(update, context):
+def bop(update):
     print(update)
     update.message.reply_text('woof')
 
