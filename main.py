cc9cc3bee3d5cf8faceffa1bd5afce306ea23a8f
RafikovTagir <RafikovTagir@gmail.com>
2022-06-20 14:54:26 +0400

    interface for choosing address

--- main.py
+++ main.py
@@ -38,23 +38,24 @@ def is_address_in_page(url, address):
         return response.text[left_p + 3:right_p]
 
 
-def check_address(update, context):
+def address_choose(update, context):
     user_id = update.message.from_user.id
     username = update.message.from_user.username
     db_object.execute(f'SELECT id FROM users WHERE id = {user_id}')
     result = db_object.fetchone()  # fetchone method returns last line of result after execution
     if not result:
-        db_object.execute('INSERT INTO users(id, username, messages) VALUES(%s, %s, %s)', (user_id, username, 0))
+        db_object.execute('INSERT INTO users(id, username, messages, address) VALUES(%s, %s, %s, %s)', (user_id, username, 0, update.message.text))
         db_connection.commit()
     db_object.execute(f'UPDATE users SET messages = messages+1 WHERE id = {user_id}')
     db_object.execute(f"UPDATE users SET address='{update.message.text}' WHERE id = {user_id}")
     db_connection.commit()
 
-    update.message.reply_text(is_address_in_page(TELASI_URL, update.message.text))
+    update.message.reply_text(f'You are chose address: {update.message.text}')
+    update.message.reply_text('If you want to check it, use /check command')
 
 
 def start(update, context):
-    update.message.reply_text('this bot can monitor websites and check for updated information on them')
+    update.message.reply_text('Hello)\n Choose address you want to check')
 
 
 def check(update, context):
@@ -69,11 +70,6 @@ def check(update, context):
         update.message.reply_text(is_address_in_page(TELASI_URL, result[0]))
 
 
-def bop(update, context):
-    print(update)
-    update.message.reply_text('woof')
-
-
 def easter_egg(update, context):
     print('Easter egg activated', update)
     print('Easter egg activated', context)
@@ -83,11 +79,10 @@ def easter_egg(update, context):
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop', bop))
     dp.add_handler(CommandHandler('Easter', easter_egg))
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('check', check))
-    dp.add_handler(MessageHandler(Filters.text, check_address))
+    dp.add_handler(MessageHandler(Filters.text, address_choose))
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
