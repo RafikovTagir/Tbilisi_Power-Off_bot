7430e2f6e36bfa216b632c63038e0f715f545c7f
RafikovTagir <RafikovTagir@gmail.com>
2022-06-25 00:10:41 +0400

    todo

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ REDIS_HOST = os.environ.get('REDIS_HOST')
 
 # TODO:
 #      2) Automatic start every morning and put request page into db
-#      3) Interface for choosing web-site and time of notification
 
 
 db_connection = psycopg2.connect(DB_URI, sslmode='require')
