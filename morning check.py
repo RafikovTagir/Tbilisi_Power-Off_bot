63802c0d536bf7f9ed42d84d4f8852ba6192f772
RafikovTagir <RafikovTagir@gmail.com>
2022-06-27 23:59:56 +0400

    good morning notification

--- morning check.py
+++ morning check.py
@@ -1,6 +1,30 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-import requests
-import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler
 import psycopg2
+import myconstants as consts
 
+
+PORT = consts.PORT
+TOKEN = consts.TOKEN
+DB_URI = consts.DB_URI
+TELASI_URL = consts.TELASI_URL
+REDIS_URL = consts.REDIS_URL
+REDIS_PORT = consts.REDIS_PORT
+REDIS_PASSWORD = consts.REDIS_PASSWORD
+REDIS_HOST = consts.REDIS_HOST
+
+
+db_connection = psycopg2.connect(DB_URI, sslmode='require')
+db_object = db_connection.cursor()
+
+
+updater = Updater(TOKEN, use_context=True)
+
+
+db_object.execute(f'SELECT id FROM users WHERE enable_notification = true')
+for record in db_object:
+    print(record)
+    updater.bot.sendMessage(chat_id=record[0], text='Good Morning from file!')
+
+
+db_connection.close()
--- myconstants.py
+++ myconstants.py
@@ -1,30 +1,11 @@
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler
-import psycopg2
-import myconstants as consts
+import os
 
 
-PORT = consts.PORT
-TOKEN = consts.TOKEN
-DB_URI = consts.DB_URI
-TELASI_URL = consts.TELASI_URL
-REDIS_URL = consts.REDIS_URL
-REDIS_PORT = consts.REDIS_PORT
-REDIS_PASSWORD = consts.REDIS_PASSWORD
-REDIS_HOST = consts.REDIS_HOST
-
-
-db_connection = psycopg2.connect(DB_URI, sslmode='require')
-db_object = db_connection.cursor()
-
-
-updater = Updater(TOKEN, use_context=True)
-
-
-db_object.execute(f'SELECT id FROM users WHERE enable_notification = true')
-for record in db_object:
-    print(record)
-    updater.bot.sendMessage(chat_id=record[0], text='Good Morning!')
-
-
-db_connection.close()
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN = os.environ.get('telegram_bot_token')
+DB_URI = os.environ.get('DATABASE_URL')
+TELASI_URL = 'http://www.telasi.ge/ru/power/'
+REDIS_URL = os.environ.get('REDIS_URL')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+REDIS_HOST = os.environ.get('REDIS_HOST')
